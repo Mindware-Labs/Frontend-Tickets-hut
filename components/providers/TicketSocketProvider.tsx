acb9825c1ef270935ec4e722ff964a76cff4fd1b
yordy2609 <yordyismael2609@gmail.com>
2026-01-06 10:03:22 -0400

    add pop up

--- components/providers/TicketSocketProvider.tsx
+++ components/providers/TicketSocketProvider.tsx
@@ -51,7 +51,7 @@ export function TicketSocketProvider() {
           <ToastAction
             altText="Ver"
             // 👇 Aquí está la actualización de la URL
-            onClick={() => router.push(`/platform/tickets?view=assigned_me&id=${data.ticketId}`)}
+            onClick={() => router.push(`/tickets?view=assigned_me&id=${data.ticketId}`)}
             className="text-blue-200 hover:text-white border-blue-200 hover:border-white"
           >
             Ver Ticket
